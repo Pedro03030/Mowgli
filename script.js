680fb91c56a4c419f3e6906fbd8a4be95bac8afe
Pedro03030 <167777469+Pedro03030@users.noreply.github.com>
2025-06-13 16:28:02 -0300

    Agrege que se pueda ver el pdf solo en español

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function showEnglish() {
   document.getElementById("english").style.display = "block";
   document.getElementById("spanish").style.display = "none";
+  document.querySelector(".pdf-section").style.display = "none"; // Oculta el PDF
 }
 
 function showSpanish() {
   document.getElementById("spanish").style.display = "block";
   document.getElementById("english").style.display = "none";
+  document.querySelector(".pdf-section").style.display = "block"; // Muestra el PDF
 }
