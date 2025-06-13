3f3260f667ad4bfe113c1ae856c343636ad0c54d
Pedro03030 <167777469+Pedro03030@users.noreply.github.com>
2025-06-13 16:17:00 -0300

    Subida inicial del proyecto Mowgli

---
+++ script.js
@@ -0,0 +1,9 @@
+function showEnglish() {
+  document.getElementById("english").style.display = "block";
+  document.getElementById("spanish").style.display = "none";
+}
+
+function showSpanish() {
+  document.getElementById("spanish").style.display = "block";
+  document.getElementById("english").style.display = "none";
+}
